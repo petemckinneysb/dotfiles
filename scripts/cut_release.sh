74b00fe1edaf1137026ec309bd9d54e83febd282
Pete McKinney <91989516+petemckinneysb@users.noreply.github.com>
2025-02-13 15:23:33 -0600

    I did stuff to this repository
    (I don't know what)

---
+++ scripts/cut_release.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+CURRENT_VERSION=$(jq -r '.version' package.json)
+NEXT_VERSION="${major}.${minor}.${patch}"
+
+IFS='.' read -r major minor patch <<< "$CURRENT_VERSION"
+minor=$((minor + 1))
+
+
+# Create release branch
+git checkout -b "release/v${CURRENT_VERSION}"
+git push -u origin HEAD --no-verify
+
+# bump develop version
+git checkout develop && git checkout -b chore/update-version-"$NEXT_VERSION"
+npm version --no-git-tag-version "$NEXT_VERSION"
+git add package.json package-lock.json && git commit -m "Update version to $NEXT_VERSION"
+git push -u origin HEAD --no-verify
+
+# Create PR
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+title=$(git branch --show-current)
+PR_BODY="Update version to $CURRENT_VERSION"
+
+gh pr create --base "develop" --head "$current_branch" --title="$title" --body "$PR_BODY"
+gh pr view --web

5e420ecd4ac06aae12085a910e85db2a1d545731
Pete McKinney <91989516+petemckinneysb@users.noreply.github.com>
2025-05-30 01:47:20 -0500

    Add wezterm correctly...

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,1 +1,14 @@
-/Users/43159/.wezterm.lua
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.initial_cols = 120
+config.initial_rows = 28
+
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 18
+config.color_scheme = "Catppuccin Mocha"
+
+config.enable_tab_bar = false
+
+return config
